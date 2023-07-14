be431fa3b0a8bf9cc94ddf1b0e2ecb7f23de55ba
CharisLai <griffin8418@gmail.com>
2023-07-14 13:47:56 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,19 @@
+// Include packages and define server related variables
+const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./routes')
+
+const app = express()
+// Define server related variables
+const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs.engine({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(routes)
+
+// Start and listen the server
+app.listen(PORT, () => {
+  console.log(`Express is running on http://localhost:${PORT}`)
+})
+
+module.exports = app
