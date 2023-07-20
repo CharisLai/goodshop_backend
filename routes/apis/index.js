e7a9a805d7bd57873eba867dc78d84fd953cec4e
CharisLai <griffin8418@gmail.com>
2023-07-20 13:49:07 +0800

    feat:api login use jwt token

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const passport = require('../../config/passport')
 const router = express.Router()
 const admin = require('./modules/admin')
 const goodshopController = require('../../controllers/apis/goodshop-controller')
+const userController = require('../../controllers/apis/user-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
 router.use('/admin', admin)
+router.post('/login', passport.authenticate('local', { session: false }), userController.login)
 // goodshop前台
 router.get('/goodshop', goodshopController.getGoodshop)
 
